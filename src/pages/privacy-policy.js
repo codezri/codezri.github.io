2ec597f924d5e9c504f95599d30ab06f9fb0e201
Shalitha Suranga <shalithasuranga@gmail.com>
2023-08-02 12:19:12 +0530

    Update privacy-policy.js

--- src/pages/privacy-policy.js
+++ src/pages/privacy-policy.js
@@ -25,15 +25,20 @@ export default function Home() {
             <div className="col col--12">
               <p>
                   We use Google Analytics in our website to understand our audience. Therefore,
-                  Google Analytics may place cookies inside your browser via the Codezri website. If you need to avoid those cookies,
-                  please disable cookies for the CodeZri website. We don't collect any personal information via
-                  web forms or any hidden mechanism. But, we ask for your email address in our contact to reply you back, and
+                  Google Analytics may place cookies inside your browser via the CodeZri website. Also, we display a few ads via Google Adsense which may 
+                  also place cookies inside your browser to improve their ad experience for users. 
+                  If you wish not to allow those cookies, please kindly disable cookies for the CodeZri website. 
+                  If you need to avoid those cookies, please disable cookies for the CodeZri website. We don't collect any personal information via
+                  web forms or any hidden mechanism. We only ask for your email address in our contact form to reply you back, and
                   we never expose your email to third party entities.
                   <br/><br/>
                   We use Google Analytics data to identify frequently visited pages and traffic sources and
                   often make tutorials and content based on those frequently visited page details and traffic sources.
                   <br/><br/>
-                  Last updated date: 2021-12-21
+                  Please note that this privacy policy statement is valid only for the official website domain `codezri.org`. 
+                  Make sure that you are browsing the right official domain name which adheres to this privacy policy statement.
+                  <br/><br/>
+                  Last updated date: 2023-08-02
               </p>
             </div>
           </div>
